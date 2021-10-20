803cba912d4740f0c784578a62674d6e70af4e55
agyllen <anton.gyllenhammar@gmail.com>
2021-10-20 15:31:57 +0200

    test

--- generator.py
+++ generator.py
@@ -108,7 +108,9 @@ def get_sentence():
     attributes_list = ['adjectives', 'animals', 'colors', 'locations']
     for attribute in attributes_list:
         words = get_words(attribute)
-        words_size = len(words[attribute])
+        print(type(words))
+        print(words)
+        words_size = len(words)
         #api_size = get_words(PREFIX, NAMESPACE, attribute)
         if words_size == 0 : #error api_size
             generated_name[attribute] = "null"
